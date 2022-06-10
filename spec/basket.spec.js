be95b9d9fec77bc02e382ff4cb856cbd93b8ebdb
xtombrock <xtombrock1989@gmail.com>
2022-06-10 10:00:16 +0100

    updates

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,115 +1,116 @@
-const Basket = require("../src/basket.js")
-describe("Basket", () => {
-    let basket
-    const smallBasket = 5;
-    const mediumBasket = 10;
-    const largeBasket = 15;
+/* eslint-disable no-undef */
+const Basket = require('../src/basket.js')
+describe('Basket', () => {
+  let basket
+  const smallBasket = 5
+  const mediumBasket = 10
+  const largeBasket = 15
 
-    beforeEach(() => {
-        basket = new Basket();
-    });
+  beforeEach(() => {
+    basket = new Basket()
+  })
 
-    //Test 1
-    it("Get all basket", () => {
-        const expected = []
-        let getBasket = basket.getBasket()
-        expect(getBasket).toEqual(expected)
-    })
+  // Test 1
+  it('Get all basket', () => {
+    const expected = []
+    const getBasket = basket.getBasket()
+    expect(getBasket).toEqual(expected)
+  })
 
-    //Test 2
-    it("Add items to basket", () => {
-        const expected = [
-            { item: "bagel", quantity: 1, price: 2.99 },
-            { item: "brownie", quantity: 3, price: 3.99 }
-        ]
-        basket.addItem("bagel", 1)
-        basket.addItem("brownie", 3)
-        let bagelInBasket = basket.getBasket()
-        expect(bagelInBasket).toEqual(expected)
-    })
+  // Test 2
+  it('Add items to basket', () => {
+    const expected = [
+      { item: 'bagel', quantity: 1, price: 2.99 },
+      { item: 'brownie', quantity: 3, price: 3.99 }
+    ]
+    basket.addItem('bagel', 1)
+    basket.addItem('brownie', 3)
+    const bagelInBasket = basket.getBasket()
+    expect(bagelInBasket).toEqual(expected)
+  })
 
-    //Test 3
-    it("Remove bagel from basket", () => {
-        const expected = this.basket = [
-            { item: "brownie", quantity: 3, price: 3.99 }]
+  // Test 3
+  it('Remove bagel from basket', () => {
+    const expected = this.basket = [
+      { item: 'brownie', quantity: 3, price: 3.99 }]
 
-        basket.addItem("bagel", 1)
-        basket.addItem("brownie", 3)
-        let removeItem = basket.removeItem("bagel")
-        expect(removeItem).toEqual(expected)
-    })
+    basket.addItem('bagel', 1)
+    basket.addItem('brownie', 3)
+    const removeItem = basket.removeItem('bagel')
+    expect(removeItem).toEqual(expected)
+  })
 
-    //Test 4
-    it("Alert when basket is full", () => {
-        const expected =
+  // Test 4
+  it('Alert when basket is full', () => {
+    const expected =
 
-            "Basket full, Please choose a bigger basket."
+            'Basket full, Please choose a bigger basket.'
 
-        basket.addItem("bagel", 3)
-        basket.addItem("brownie", 5)
-        let alert = basket.basketCapacity()
-        expect(alert).toEqual(expected)
-    })
+    basket.addItem('bagel', 3)
+    basket.addItem('brownie', 5)
+    const alert = basket.basketCapacity()
+    expect(alert).toEqual(expected)
+  })
 
-    //Test 5
-    it("Create basket with larger size", () => {
-        const expected = this.basketSize = largeBasket
+  // Test 5
+  it('Create basket with larger size', () => {
+    const expected = this.basketSize = largeBasket
 
-        new Basket(largeBasket)
-        let checkSize = this.basketSize
-        expect(checkSize).toEqual(expected)
-    })
+    new Basket(largeBasket)
+    const checkSize = this.basketSize
+    expect(checkSize).toEqual(expected)
+  })
 
-    //Test 6
-    it("Alert when trying to remove item that doesnt exist inside basket", () => {
-        const expected = "This item is not in the basket."
+  // Test 6
+  it('Alert when trying to remove item that doesnt exist inside basket', () => {
+    const expected = 'This item is not in the basket.'
 
-        basket.addItem("bagel", 3)
-        basket.addItem("brownie", 5)
-        let alert = basket.removeItem("Kebab", 10)
-        expect(alert).toEqual(expected)
-    })
+    basket.addItem('bagel', 3)
+    basket.addItem('brownie', 5)
+    const alert = basket.removeItem('Kebab', 10)
+    expect(alert).toEqual(expected)
+  })
 
-    //Test 7 
-    it("price checker for items", () => {
-        const expected = 3.99
+  // Test 7
+  it('price checker for items', () => {
+    const expected = 3.99
 
-        basket.priceChecker("brownie")
-        let checkPrice = basket.priceChecker("brownie")
-        expect(checkPrice).toEqual(expected)
-    })
+    basket.priceChecker('brownie')
+    const checkPrice = basket.priceChecker('brownie')
+    expect(checkPrice).toEqual(expected)
+  })
 
-    //Test 8
-    it("favourite bagel quantity", () => {
-        const expected = 3
-        basket.addItem("chocolateBagel", 1)
-        basket.addItem("chocolateBagel", 1)
-        basket.addItem("brownie", 1)
-        basket.addItem("chocolateBagel", 1)
-        let alert = basket.variantTotal("chocolateBagel")
-        expect(alert).toEqual(expected)
-    })
+  // Test 8
+  it('favourite bagel quantity', () => {
+    const expected = 3
+    basket.addItem('chocolateBagel', 1)
+    basket.addItem('chocolateBagel', 1)
+    basket.addItem('brownie', 1)
+    basket.addItem('chocolateBagel', 1)
+    const alert = basket.variantTotal('chocolateBagel')
+    expect(alert).toEqual(expected)
+  })
 
-    //Test 9
-    it("basket total", () => {
-        const expected = "£29.93"
+  // Test 9
+  it('basket total', () => {
+    const expected = '£29.93'
 
-        basket.addItem("chocolateBagel", 3)
-        basket.addItem("bagel", 1)
-        basket.addItem("brownie", 3)
-        let total = basket.basketTotal()
-        expect(total).toEqual(expected)
-    })
+    basket.addItem('chocolateBagel', 3)
+    basket.addItem('bagel', 1)
+    basket.addItem('brownie', 3)
+    const total = basket.basketTotal()
+    expect(total).toEqual(expected)
+  })
 
-    //Test 10
-    it("Throw error if negative quanitity", () => {
-        const expected = "Cant add negative numbers"
-        let bagelInBasket = basket.addItem("bagel", -1)
-        expect(bagelInBasket).toEqual(expected)
-    })
+  // Test 10
+  it('Throw error if negative quanitity', () => {
+    const expected = 'Cant add negative numbers'
+    const bagelInBasket = basket.addItem('bagel', -1)
+    expect(bagelInBasket).toEqual(expected)
+  })
 
-    //Test 11
-    //it('item not in menu',() =>{
+  // Test 11
+  // it('item not in menu',() =>{
 
-    //})
+  // })
 })
--- src/basket.js
+++ src/basket.js
@@ -1,91 +1,79 @@
 
-
 // these become variable 'capacity'
-const smallBasket = 5;
-const mediumBasket = 10;
-const largeBasket = 15;
+const smallBasket = 5
+const mediumBasket = 10
+const largeBasket = 15
 
 class Basket {
+  // set to default
+  constructor (capacity = smallBasket) {
+    this.basket = []
+    this.basketSize = capacity
+    this.menu = {
+      bagel: 2.99,
+      brownie: 3.99,
+      chocolateBagel: 4.99
+    }
+  }
 
-    // set to default
-    constructor(capacity = smallBasket) {
-        this.basket = []
-        this.basketSize = capacity
-        this.menu ={
-            bagel: 2.99,
-            brownie: 3.99,
-            chocolateBagel: 4.99 
-        }
+  getBasket () {
+    return this.basket
+  }
 
-
+  addItem (itemName, itemQuantity) {
+    if (!(itemName in this.menu)) {
+      return 'not in menu'
+    } else if (itemQuantity > 0) {
+      const insideBasket = {
+        item: itemName,
+        quantity: itemQuantity,
+        price: this.menu[itemName]
+      }
+      this.basket.push(insideBasket)
+      return true
     }
 
-    getBasket() {
+    return 'Cant add negative numbers'
+  }
+
+  removeItem (itemName) {
+    for (let i = 0; i < this.basket.length; i++) {
+      if (this.basket[i].item === itemName) {
+        this.basket.splice(i, 1)
         return this.basket
-    } 
-
-    addItem(itemName, itemQuantity) {
-        if (!(itemName in this.menu)){
-            return 'not in menu'
-        }
-        
-           else if (itemQuantity > 0) {
-                const insideBasket = {
-                    item: itemName,
-                    quantity: itemQuantity,
-                    price: this.menu[itemName]
-                }
-                this.basket.push(insideBasket)
-                return true
-                
-            }
-        
-        return "Cant add negative numbers"
+      }
     }
+    return 'This item is not in the basket.'
+  }
 
-    removeItem(itemName) {
-        for (let i = 0; i < this.basket.length; i++) {
-            if (this.basket[i].item === itemName) {
-                this.basket.splice(i, 1)
-                return this.basket
-            }
-        }
-        return "This item is not in the basket."
-    }
+  basketCapacity () {
+    // reduce gets total of array
+    const totalCapacity = this.basket.reduce((total, quantity) => { return total + quantity.quantity }, 0)
+    if (totalCapacity > this.basketSize) { return 'Basket full, Please choose a bigger basket.' }
+  }
 
-    basketCapacity() {
-        // reduce gets total of array
-        const totalCapacity = this.basket.reduce((total, quantity) => { return total + quantity.quantity }, 0)
-        if (totalCapacity > this.basketSize) 
-            return "Basket full, Please choose a bigger basket."
-        
-    }
+  priceChecker (itemName) {
+    return this.menu[itemName]
+  }
 
-    priceChecker(itemName) {
-       
-        return this.menu[itemName]
-            
+  basketTotal () {
+    const eachItem = []
+    for (let i = 0; i < this.basket.length; i++) {
+      eachItem.push(this.basket[i].quantity * this.basket[i].price)
     }
+    const totalPrice = eachItem.reduce((total, quantity) => { return total + quantity }, 0)
+    return ('£' + totalPrice)
+  }
 
-    basketTotal() {
-        let eachItem = []
-        for (let i = 0; i < this.basket.length; i++) {
-            eachItem.push(this.basket[i].quantity * this.basket[i].price)
-        }
-        const totalPrice = eachItem.reduce((total, quantity) => { return total + quantity }, 0)
-        return ("£" + totalPrice)
+  variantTotal (variant) {
+    let itemTotal = 0
+    for (let i = 0; i < this.basket.length; i++) {
+      if (this.basket[i].item === variant) {
+        itemTotal++
+      }
     }
-
-    variantTotal(variant) {
-        let itemTotal = 0
-        for (let i = 0; i < this.basket.length; i++) {
-            if (this.basket[i].item === variant) {
-                itemTotal++
-            }
-        }
-        return itemTotal
-    }
+    return itemTotal
+  }
 }
-
 
 module.exports = Basket
